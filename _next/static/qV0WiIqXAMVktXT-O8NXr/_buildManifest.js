15d411e3e44bbbc635e58b0607692bbc92096b7d
Neil Irvine Morales <morales.neil.irvine.20@gmail.com>
2026-01-15 18:06:10 +0800

    Updates

---
+++ _next/static/qV0WiIqXAMVktXT-O8NXr/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portolfio//_next/:path+",
+        "destination": "/portolfio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
